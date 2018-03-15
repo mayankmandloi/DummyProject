168ed4a3e967981175881ccc3f45aefa05e44f38
mayankmandloi <mayankmandloi009@gmail.com>
2018-03-15 15:41:40 +0530

    as

--- Mayank/js/script.js
+++ Mayank/js/script.js
@@ -2,11 +2,12 @@ var app =angular.module("app",["ngRoute"]);
 app.config(function($routeProvider){
 $routeProvider.when("/",{
     templateUrl:"templates/bookSearch.html",
-    controller:function($scope,$http){
+    controller:function($scope,$http,bookBox){
         $scope.search=function(){
              $http.get("http://it-ebooks-api.info/v1/search/"+$scope.searchText).then(function success (done){
                  console.log(done);
             $scope.Books=done.data.Books;
+            bookBox.Books=done.data.Books;
     },function failuer(resData)
     {
         console.log("Sorry No Data");
@@ -14,4 +15,15 @@ $routeProvider.when("/",{
     }
     }
 })
+
+$routeProvider.when("/book/:ID",{
+    templateUrl:"templates/bookview.html",
+    controller:function($scope,$routeParams,bookBox){
+        $scope.id=$routeParams.ID;
+        $scope.Books=bookBox.Books;
+        console.log($routeParams);
+    }
+})
 });
+
+app.service("bookBox",function(){});
